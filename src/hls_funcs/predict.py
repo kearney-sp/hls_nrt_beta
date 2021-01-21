2f1c068f7bd392126f4ed3c15c66697b84469340
Kearney <12001003846801@FEDIDCARD.GOV>
2021-01-21 15:15:41 -0700

    update predict functions and hls_biomass notebook

--- src/hls_funcs/predict.py
+++ src/hls_funcs/predict.py
@@ -45,3 +45,40 @@ def predict_biomass(dat, model, se=True):
     else:
         return xr.DataArray(data=new_df['bm'].values.reshape(dat[list(dat.keys())[0]].shape),
                         coords=dat.coords)
+
+
+def pred_bm(dat, model, dim):
+    model_vars = [n for n in model.params.index if ":" not in n and "Intercept" not in n]
+
+    dat_masked = dat.where(dat.notnull)
+
+    dims_list = [[dim] for v in model_vars]
+
+    def pred_func(*args, mod_vars_np):
+        vars_dict_np = {}
+        for idx, v in enumerate(mod_vars_np):
+            vars_dict_np[v] = args[idx]
+        #print(vars_dict_np)
+        bm_np = np.ones_like(args[0]) * np.nan
+        mask = np.any(np.isnan(args), axis=0)
+        bm_np[~mask] = np.exp(model.predict(vars_dict_np))
+        #print(bm_np)
+        return bm_np.astype('int16')
+
+    def pred_func_xr(dat_xr, model_vars_xr, dims):
+        vars_list_xr = []
+        for v in model_vars_xr:
+            vars_list_xr.append(func_dict[v](dat_xr))
+        bm_xr = xr.apply_ufunc(pred_func,
+                               *vars_list_xr,
+                               kwargs=dict(mod_vars_np=np.array(model_vars_xr)),
+                               dask='parallelized',
+                               vectorize=True,
+                               input_core_dims=dims,
+                               output_core_dims=[dims[0]],
+                               output_dtypes=['int16'])
+        return bm_xr
+
+    bm_out = pred_func_xr(dat_masked, model_vars, dims_list)
+
+    return bm_out
